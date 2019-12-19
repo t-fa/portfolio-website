723031ffa377335c6b846393abce78f8b6e602b2
t-fa <thomas.fattah@gmail.com>
2019-12-19 12:45:01 -0500

    Commented out error routes temporarily

--- app.js
+++ app.js
@@ -5,16 +5,21 @@ var express = require("express"),
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
+app.set('port', 8080);
 
-app.use(function(req,res){
-    res.status(404);
-    res.render('404');
+app.get('/', function(req, res){
+    res.render("index");
 });
 
-app.use(function(req,res){
-  res.status(500);
-  res.render('500');
-});
+// app.use(function(req,res){
+//     res.status(404);
+//     res.render('404');
+// });
+
+// app.use(function(req,res){
+//   res.status(500);
+//   res.render('500');
+// });
 
 app.listen(app.get('port'), function(){
     console.log('Server is running on ' + app.get('port') + '; press Ctrl-C to terminate.');
