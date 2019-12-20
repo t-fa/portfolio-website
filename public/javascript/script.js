57f32e0b26c50365f6b0c73196cf53316d391e59
t-fa <thomas.fattah@gmail.com>
2019-12-19 20:36:13 -0500

    bearMode now checks if element exists since same script is used on multiple pages

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,13 +1,25 @@
 function bearMode(){
     var bare = document.getElementById("bare");
-    bare.textContent = "bear";
+    if(bare){
+        bare.textContent = "bear";
+    }
     var bearimg = "https://images.unsplash.com/photo-1530595467537-0b5996c41f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80";
     var p = document.getElementById("mainp");
-    p.textContent = "This is me thinking about computers:";
+    if(p){
+        p.textContent = "This is me thinking about computers:";
+    }
     var img = document.getElementById("mainimg");
-    img.src = bearimg;
+    if(img){
+        img.src = bearimg;
+    }
     var name = document.getElementById("name");
-    name.textContent = "a bear";
+    if(name){
+        name.textContent = "a bear";
+    }
+    var contact = document.getElementById("contacthello");
+    if(contact){
+        contact.textContent = "Send honey!";
+    }
 }
 
 document.getElementById("bearmode").addEventListener('click', bearMode);
