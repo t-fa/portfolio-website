f0ae710671be37514d6a5524462ca2b69d1b6cc7
t-fa <thomas.fattah@gmail.com>
2019-12-23 13:59:45 -0500

    Added client side input validation

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,3 +1,4 @@
+// DOM manipulation for bear mode
 function bearMode(){
     var bare = document.getElementById("bare");
     if(bare){
@@ -22,34 +23,87 @@ function bearMode(){
     }
 }
 
+// sends an AJAX request to nodemailer to send an email and validates input
 function contactForm(){
     var submitbutton = document.getElementById('submitbutton');
     if(submitbutton){
         submitbutton.addEventListener('click', function(event){
             var req = new XMLHttpRequest();
+            var span = document.getElementById("response");
             var payload = {};
             payload.name = document.getElementById("contactname").value;
             payload.email = document.getElementById("contactemail").value;
             payload.subject = document.getElementById("contactsubject").value;
             payload.body = document.getElementById("contactbody").value;
-            var span = document.getElementById("response");
-            req.open('POST', "/", true);
-            req.setRequestHeader('Content-Type', 'application/json');
-            req.addEventListener('load',function(){
-                if(req.status >= 200 && req.status < 400){
-                    span.textContent = "Email successfully sent!";
-                    document.getElementById("contactname").value = "";
-                    document.getElementById("contactemail").value = "";
-                    document.getElementById("contactsubject").value = "";
-                    document.getElementById("contactbody").value = "";
-                } else {
-                    span.textContent = "An error occurred. Please try again.";
-                }
-            });
-            req.send(JSON.stringify(payload));
+            if(validateInput()){
+                req.open('POST', "/", true);
+                req.setRequestHeader('Content-Type', 'application/json');
+                req.addEventListener('load',function(){
+                    if(req.status >= 200 && req.status < 400 && inputVerified){
+                        span.textContent = "Email successfully sent!";
+                        document.getElementById("contactname").value = "";
+                        document.getElementById("contactemail").value = "";
+                        document.getElementById("contactsubject").value = "";
+                        document.getElementById("contactbody").value = "";
+                    } else {
+                        span.textContent = "An error occurred. Please try again.";
+                    }
+                });
+                req.send(JSON.stringify(payload));
+            }
             event.preventDefault();
         });
     }
+}
+
+function validateInput(){
+    var span = document.getElementById("response");
+    var payload = {};
+    payload.name = document.getElementById("contactname").value;
+    payload.email = document.getElementById("contactemail").value;
+    payload.subject = document.getElementById("contactsubject").value;
+    payload.body = document.getElementById("contactbody").value;
+
+    // check for null
+    if(payload.name === ""){
+        span.textContent = "Name cannot be blank! ";
+        span.classList.add("error");
+        return false;
+    }
+    if(payload.email === ""){
+        span.textContent = "Email cannot be blank! ";
+        span.classList.add("error");
+        return false;
+    }
+    if(payload.subject === ""){
+        span.textContent = "Subject cannot be blank! ";
+        span.classList.add("error");
+        return false;
+    }
+    if(payload.body === ""){
+        span.textContent = "Body cannot be blank! ";
+        span.classList.add("error");
+        return false;
+    }
+
+    // check for valid email
+    if(!validateEmail(payload.email)){
+        span.textContent = "Please enter a valid email!";
+        span.classList.add("error");
+        return false;
+    }
+
+    if(payload.name && payload.email && payload.subject && payload.body){
+        span.textContent = "";
+        span.classList.remove("error");
+        return true;
+    }
+}
+
+// https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
 
 if(document.getElementById("submitbutton")){
