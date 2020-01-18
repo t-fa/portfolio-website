042484a7a4546c4efda4a48b5b844e0b0dee493c
t-fa <thomas.fattah@gmail.com>
2020-01-17 20:12:49 -0500

    Changed App to class and Header to function

--- pfreact/src/App.js
+++ pfreact/src/App.js
@@ -1,28 +1,50 @@
 import React from 'react';
 
-function App() {
-  return (
-    <Header />
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bearMode: false
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Introduction name="Tom Fattah" />
+      </div>
+    );
+  } 
 }
 
-class Header extends React.Component {
+function Header() {
+  return (
+    <div className="header">
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/contact">Contact</a></li>
+        <li id="bearmode"><a>Bear Mode</a></li>
+      </ul>
+      <a href="https://github.com/t-fa/">
+      <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" />
+      </a>
+    </div>
+  );
+};
+
+class Introduction extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
-      <div className="header">
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/contact">Contact</a></li>
-          <li id="bearmode"><a>Bear Mode</a></li>
-        </ul>
-        <a href="https://github.com/t-fa/">
-        <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" />
-        </a>
-      </div>		
-    );
+      <div className="introduction">
+        <h1>Hi there! I'm {this.props.name}</h1>
+        <h2>I build things with my bare hands...using computer science!</h2>
+        <p>I can build a custom tailed solution for you using the latest technologies.</p>
+        <img className="introimg" src="https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80" />
+      </div>
+    )
   }
 };
 
